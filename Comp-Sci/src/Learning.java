6d5c5b71ba1a7610c040e2f6719fbdcf4833ffff
Duy Nguyen <punkmeister@protonmail.com>
2020-06-25 18:16:34 -0700

    finally

---
+++ Comp-Sci/src/Learning.java
@@ -0,0 +1,22 @@
+public class Learning {
+    public static void main(String[] args) {
+                /*
+        if (time1 < time2) {
+            hours = (int)(time2/100) - (int)(time1/100) - 1;
+            minutes = ((time2 % 100) + (60 - (time1 % 100)));
+            interval = (hours * 60) + minutes;
+        }
+        else if (time1 > time2) {
+            hours = (24 - (int)(time1/100)) + (int)(time2/100) - 1;
+            minutes = ((time2 % 100) + (60 - (time1 % 100)));
+            interval = (hours * 60) + minutes;
+        }
+        else if (time1 == time2) {
+            interval = 1440;
+        }
+        else {
+            System.out.println("Error, input times are not valid");
+        }
+         */
+    }
+}
