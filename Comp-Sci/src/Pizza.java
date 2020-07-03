7aaa90e9d1b114b9e7ee1352e860c554d259defa
Duy Nguyen <punkmeister@protonmail.com>
2020-07-03 11:07:21 -0700

    pizza corrections

--- Comp-Sci/src/Pizza.java
+++ Comp-Sci/src/Pizza.java
@@ -9,6 +9,10 @@ import java.util.Scanner;
 public class Pizza {
     public static void main(String[] args) {
 
+        final double SMALLPIZZA = 7.25;
+        final double MEDIUMPIZZA = 10.50;
+        final double LARGEPIZZA = 14.75;
+
         Scanner input = new Scanner(System.in);
 
         // Takes input and repeats prompt if input is invalid
@@ -24,11 +28,11 @@ public class Pizza {
 
             if (area <= 0) {
                 System.out.printf("The area (%.2f) " +
-                        "must be greater than zero.\n" ,area);
+                        "must be greater than zero.\n", area);
             }
             else if (area > 1000) {
                 System.out.printf("The area (%.2f) " +
-                        "must be less than or equal to 1000.\n" ,area);
+                        "must be less than or equal to 1000.\n", area);
             }
         } while ((area <= 0) || (area > 1000));
 
@@ -46,26 +50,22 @@ public class Pizza {
         String size;
         double price;
 
-        double smallPizza = 7.25;
-        double mediumPizza = 10.50;
-        double largePizza = 14.75;
-
         System.out.printf("\nThe area of your pizza " +
                 "is %.2f square cm.\n", area);
         if (area <= 225) {
             size = "small";
-            price = smallPizza;
+            price = SMALLPIZZA;
         }
         else if (area <= 650) {
             size = "medium";
-            price = mediumPizza;
+            price = MEDIUMPIZZA;
         }
         else {
             size = "large";
-            price = largePizza;
+            price = LARGEPIZZA;
         }
 
-        price += (toppings * 0.85);
+        price += toppings * 0.85;
 
         System.out.printf("This is a %s pizza.\n", size);
         System.out.printf("Your pizza costs %.2f euros.\n", price);
