c0d2f794e65d0c69495fd6a91adffd6e41bf7a7e
Duy Nguyen <punkmeister@protonmail.com>
2020-07-22 09:17:51 -0700

    fixes

--- Comp-Sci/src/CityInfo.java
+++ Comp-Sci/src/CityInfo.java
@@ -49,34 +49,39 @@ class City {
 public class CityInfo {
 
     // Reads city file lines that have proper format
-    public static ArrayList<City> readCityFile(File fileName) throws FileNotFoundException {
+    public static ArrayList<City> readCityFile(File fileName) {
 
-        Scanner file = new Scanner(fileName);
         ArrayList<City> cityList = new ArrayList<>();
 
-        while (file.hasNextLine()) {
+        try {
+            Scanner file = new Scanner(fileName);
+            while (file.hasNextLine()) {
 
-            String line = file.nextLine();
-            String [] properties = line.split(";");
+                String line = file.nextLine();
+                String[] properties = line.split(";");
 
-            if (properties.length != 3) {
-                System.out.printf("\"%s\" does not have three entries.\n",
-                        line);
-            }
-            else {
-                try {
-                    cityList.add(new City (
-                            properties[0],
-                            properties[1],
-                            Integer.valueOf(properties[2])));
+                if (properties.length != 3) {
+                    System.out.printf("\"%s\" does not have three entries.\n",
+                            line);
+                } else {
+                    try {
+                        cityList.add(new City(
+                                properties[0],
+                                properties[1],
+                                Integer.valueOf(properties[2])));
+                    }
+                    catch (NumberFormatException ex) {
+                        System.out.printf("\"%s\" does not have correct data type(s). \n", line);
+                    }
                 }
-                catch (NumberFormatException ex) {
-                    System.out.printf("\"%s\" does not have correct data type(s). \n", line);
-                }
             }
+
+            file.close();
+        }
+        catch (FileNotFoundException ex) {
+            System.out.printf("File, %s, could not be opened or found ...\n", fileName);
         }
 
-        file.close();
         return cityList;
     }
 
@@ -132,7 +137,7 @@ public class CityInfo {
         File cities = new File("citylist.dat");
         boolean exit = false;
 
-        System.out.println("Reading city file ...");
+        System.out.println("Reading city file ...\n");
         ArrayList<City> cityList = readCityFile(cities);
 
         System.out.println(); // Formatting
