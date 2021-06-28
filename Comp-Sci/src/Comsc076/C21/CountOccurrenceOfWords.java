595d1164ca385d736d19050999c37fad9bedb7c1
PunkmeisterDuy <contact@duy-nguyen.com>
2021-06-27 19:13:11 -0700

    hello

--- Comp-Sci/src/Comsc076/C21/CountOccurrenceOfWords.java
+++ Comp-Sci/src/Comsc076/C21/CountOccurrenceOfWords.java
@@ -15,6 +15,8 @@ public class CountOccurrenceOfWords {
 
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
 
     }
 }
